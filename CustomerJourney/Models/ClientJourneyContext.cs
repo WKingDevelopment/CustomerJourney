097e099e210f06ab2b0dce181e1912eab7184ef5
Will <WilliamDKing96@gmail.com>
2020-12-15 18:39:34 +0000

    Creating Models & getting rid of example files & adding example EF model building code

---
+++ CustomerJourney/Models/ClientJourneyContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace CustomerJourney.Models
+{
+    public partial class ClientJourneyContext : DbContext
+    {
+        public ClientJourneyContext()
+        {
+        }
+
+        public ClientJourneyContext(DbContextOptions<ClientJourneyContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ConfigPhase> ConfigPhases { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CustomerJourney;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<ConfigPhase>(entity =>
+            {
+                entity.HasKey(e => e.CompanyId)
+                    .HasName("PK__Config_P__2D971C4C528769AD");
+
+                entity.ToTable("Config_Phases");
+
+                entity.Property(e => e.CompanyId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("CompanyID");
+
+                entity.Property(e => e.Phases)
+                    .IsRequired()
+                    .HasMaxLength(500);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CustomerJourney/Models/ConfigPhase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CustomerJourney.Models
+{
+    public partial class ConfigPhase
+    {
+        public int CompanyId { get; set; }
+        public string Phases { get; set; }
+    }
+}
