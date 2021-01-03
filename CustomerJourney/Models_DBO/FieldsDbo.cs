4da3bcf9c00d807de093919362a4a3a9779f6a81
Will <WilliamDKing96@gmail.com>
2021-01-03 17:15:50 +0000

    Fixing Fields Not Appearing Test: Working

--- CustomerJourney/Models_DBO/FieldsDbo.cs
+++ CustomerJourney/Models_DBO/FieldsDbo.cs
@@ -10,6 +10,7 @@ namespace CustomerJourney.Models_DBO
     {
         public int id { get; set; }
         public int seq { get; set; }
+        public string size { get; set; }
         public bool summary { get; set; }
     }
 }
