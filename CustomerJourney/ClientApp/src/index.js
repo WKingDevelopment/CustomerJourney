0b15e13b34cfe94ac05655fdc4d364b96283d0af
Will <WilliamDKing96@gmail.com>
2021-02-14 15:11:25 +0000

    Resolving dependencies

--- CustomerJourney/ClientApp/src/index.js
+++ CustomerJourney/ClientApp/src/index.js
@@ -1,8 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import {App} from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
